e9a77a12da276dc44690599ef0832f26ae482661
Diogo Machado <diogo@diogomachado.com>
2022-05-26 19:17:50 -0300

    Removed animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,15 @@
 import React from "react";
 import { Quote } from "./components/Quote";
 import "./App.css";
-import { useSpring, animated } from "react-spring";
 
 function App() {
-  const props = useSpring({ to: { opacity: 1 }, from: { opacity: 0 } });
-
   return (
-    <animated.div style={props}>
-      <div className="grid-quotes">
-        <Quote text="Code!" />
-        <Quote text="HTML" />
-        <Quote text="CSS" />
-        <Quote text="JS" />
-      </div>
-    </animated.div>
+    <div className="grid-quotes">
+      <Quote text="Code!" />
+      <Quote text="HTML" />
+      <Quote text="CSS" />
+      <Quote text="JS" />
+    </div>
   );
 }
 
